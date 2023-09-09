4a71bd2adde41db581f52a9977df40f1d57635ae
Navaty Khara <navatykhara@gmail.com>
2023-09-08 23:00:51 -0400

    Removing unnessecary Card attributes

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -10,7 +10,7 @@ interface CardInterface {
 const MyCard = ({ title, cover, url }: CardInterface) => {
   return (
 	<Link href={url} isExternal >
-		<Card class="p-4 m-8 rounded">
+		<Card>
 			<Image
 				alt="Card background"
 				className="object-cover rounded-xl"
